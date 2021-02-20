22656d159ecfc58e0a90a19a368a7f6d5e68e029
Mykhail <gelios238@gmail.com>
2021-02-20 10:05:48 +0200

    it cumasutra1

---
+++ src/components/Profile.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Profile = () => {
+	return (
+		<div className='content'>
+			<div className='app_main_img'><img src='https://peakvisor.com/img/news/french-mountains.jpg'/></div>
+		 </div>
+	)
+}
+export default Profile;
