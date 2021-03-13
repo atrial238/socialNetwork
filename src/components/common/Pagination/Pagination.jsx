74863f7feef0f03aad4a8aa049b6fe6bca35c52d
Mykhail <gelios238@gmail.com>
2021-03-13 17:05:05 +0200

    refactoring Pagination.js

--- src/components/common/Pagination/Pagination.jsx
+++ src/components/common/Pagination/Pagination.jsx
@@ -1,29 +1,43 @@
 import React, {useState} from 'react';
 import {pagination, currentPageClass} from './Pagination.module.css';
 
-const Pagination = ({totalItems, itemsPerPage, numberCurrentPage, setPage}) => {
+const Pagination = ({totalItems, itemsPerPage, numberCurrentPage, setPage, amountVisiblePortion = 9}) => {
 
-	// const Math.ceil(totalItems / itemsPerPage)
+	const [numberPortionVisibleItmes, setVisibleItems] = useState(1);
 
-	const numbersPages = Array.from({length: 9}).map((element, index) => 
-									<li 
-										key={index}
-										className={numberCurrentPage === index + 1 ? currentPageClass : null}
-										onClick={() => setPage(index + 1)}
-									>{index + 1}</li>
+	const amountAllItmes = Math.ceil(totalItems / itemsPerPage),
+		leftVisibleItems = (numberPortionVisibleItmes - 1) * amountVisiblePortion,
+		rightVisibleItems = numberPortionVisibleItmes * amountVisiblePortion - 1 ;
+
+	const changeVisibleItems = (direction) => {
+		if(direction){
+			setVisibleItems(numberPortionVisibleItmes + 1)
+			setPage(leftVisibleItems + amountVisiblePortion + 1)
+		}else{
+			setVisibleItems(numberPortionVisibleItmes - 1);
+			setPage(leftVisibleItems - (amountVisiblePortion - 1))
+		}
+	}
+
+	const portionVisibleItems = Array
+											.from({length: amountAllItmes})
+											.reduce((arr, elem, index) => index  >= leftVisibleItems && index <= rightVisibleItems ? [...arr, index + 1] : arr, [])
+											.map((elem) => 
+												<li 
+													key={elem}
+													className={numberCurrentPage === elem ? currentPageClass : null}
+													onClick={() => setPage(elem)}
+												>{elem}</li>
 	);
-
+	
 	return (
-		
 		<div className={pagination}>
-			<button>Prev</button>
+			{leftVisibleItems >= 1 && <button onClick={() => changeVisibleItems(false)} >Prev</button>}
 			<ul>
-				{numbersPages}
+				{portionVisibleItems}
 			</ul>
-			<button>Next</button>
+			<button onClick={() =>changeVisibleItems(true) }>Next</button>
 		</div>
 	)
-	
 } 
-
 export default Pagination;
