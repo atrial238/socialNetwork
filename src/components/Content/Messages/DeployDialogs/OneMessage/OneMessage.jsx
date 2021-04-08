41615f788302ddba05b2028ac1005d84d7d61199
Mykhail <gelios238@gmail.com>
2021-04-08 08:11:24 +0300

    add avatar placeholder

--- src/components/Content/Messages/DeployDialogs/OneMessage/OneMessage.jsx
+++ src/components/Content/Messages/DeployDialogs/OneMessage/OneMessage.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import {wrapper, body_message, avatar, auth_user_style, avatar_wrapper}from './OneMessage.module.scss';
+import placeHolderAvatar from '../../../../../assets/images/avatar/placeholder_avatar.jpg';
 
 const OneMessage = ({text, pathImg, isAuthUser, id}) => {
 	
@@ -8,7 +9,7 @@ const OneMessage = ({text, pathImg, isAuthUser, id}) => {
 		<div className={`${wrapper} ${(isAuthUser && auth_user_style) || ''}`}>
 			<div className={avatar}>
 				<NavLink to={'/profile/' + id}>
-					<div className={avatar_wrapper}><img src={pathImg}/></div>
+					<div className={avatar_wrapper}><img src={pathImg || placeHolderAvatar}/></div>
 				</NavLink>
 				
 			</div>
