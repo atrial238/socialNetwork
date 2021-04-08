56b67d5c46e108dc5e34fe1d471d25ea4db01e69
Mykhail <gelios238@gmail.com>
2021-04-08 09:34:03 +0300

    adding mobile adaptation

--- src/components/common/RenderFormField/RenderFormField.jsx
+++ src/components/common/RenderFormField/RenderFormField.jsx
@@ -4,10 +4,11 @@ import PropTypes from 'prop-types';
 
 const RenderField = ({meta, input, typeTag, placeholder = '', errorMessage}) => {
 	
+	
 	return (
 		<div className={`${container} ${meta.touched && meta.error && errorBorder}`}>
 			{React.createElement(typeTag, {...input, placeholder:placeholder}, null)}
-			{ meta.touched && meta.error && <span className={requireStyleError}><span>{errorMessage}</span></span>}
+			{meta.touched && meta.error && <span className={requireStyleError}><span>{errorMessage}</span></span>}
 		</div>
 	)
 }
