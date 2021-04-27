4abbfb25b95f7a5635c4c8c5e9f7b6e1fc2075a0
Mykhail Bobrykov <gelios238@gmail.com>
2021-04-27 17:22:39 +0300

    typed NameUser

---
+++ src/components/Content/Profile/HeaderProfile/NameUser/NameUser.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import UserStatus from './UserStatus/UserStatus';
+import style from './NameUser.module.scss';
+
+const {wrapper, name_user} = style;
+interface PropsNameUserType {
+	nameUser: string
+	userStatus: string
+	updateStatus: Function
+	isOwner: boolean
+}
+const NameUser: React.FC<PropsNameUserType> = ({nameUser, ...userStatusProps}) => {
+
+	return (
+		<div className={wrapper}>
+			<h2 className={name_user}>{nameUser}</h2>
+			<UserStatus {...userStatusProps}/>
+		</div>
+	)
+}
+export default NameUser;
